87c2762f78d4d137266ab51cb6186b99fdd8f404
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-26 10:10:13 +0700

    Add project files.

---
+++ Parameter/Parameter.cpp
@@ -0,0 +1,23 @@
+// Parameter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int luas;
+
+void prosedureLuas(int p, int l)
+{
+	luas = p * l;
+}
+int fungsiLuas(int p, int l)
+{
+	return p * l;
+}
+
+
+int main()
+{
+   
+}
+
